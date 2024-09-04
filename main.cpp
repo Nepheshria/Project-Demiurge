30e678553fc0490b274f8c24235e521fa26305cc
Villerot Justin <justinvillerot@orange.fr>
2024-09-04 18:52:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,10 @@
 #include "stb_image.h"
 #include <cmath>
 
+// GLM Maths imports
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 float ratio = 0.2f;
 
@@ -27,6 +30,12 @@ void processInput(GLFWwindow *window)
 }
 
 int main() {
+
+    glm::vec4 vec(1.0f, 0.0f, 0.0f, 1.0f);
+    glm::mat4 trans = glm::mat4(1.0f);
+    trans = glm::translate(trans, glm::vec3(1.0f, 1.0f, 0.0f));
+    vec = trans * vec;
+    std::cout << vec.x << vec.y << vec.z << std::endl;
 
     // glfw: initialize and configure
     // ------------------------------
